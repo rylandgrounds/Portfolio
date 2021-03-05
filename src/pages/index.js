6c9d04fdf3482ccb8e3e5c5abb828160e2b83629
Ryland <rylandgrounds@gmail.com>
2021-03-04 21:48:03 -0600

    fixes and updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Ryland Ground\'s PortFolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Ryland Ground\'s PortFolio'} />
       </Helmet>
       <App />
     </>
