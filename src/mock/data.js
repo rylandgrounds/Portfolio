65360454b1f29de608189ba0efc86faf1eda3cb1
Ryland <rylandgrounds@gmail.com>
2021-03-09 20:36:54 -0600

    bug fix

--- src/mock/data.js
+++ src/mock/data.js
@@ -96,5 +96,10 @@ export const footerData = {
     },
   ],
 };
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
+
 
 
